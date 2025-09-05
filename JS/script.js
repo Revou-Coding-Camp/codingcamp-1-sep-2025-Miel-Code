ed7d6485368c7834d1596e5cd8f4a41af762cf39
Miel-Code <jilanrokan@gmail.com>
2025-09-06 00:32:59 +0700

    Refactor task management UI and implement input validation for better user experience

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,45 @@
-function validateInput(){
-    const inputField = document.getElementById("taskInput").value;
-    const dateField = document.getElementById("dateInput").value;
+let todo = JSON.parse(localStorage.getItem("todo")) || [];
 
-    console.log('todo', inputField);
-    console.log('due date', dateField);
+const taskInput = document.getElementById("taskInput");
+const dateInput = document.getElementById("dateInput");
+const addButton = document.querySelector(".btn");
+const taskList = document.getElementById("taskList");
+const btnFilter = document.getElementById("btn-filter");
+const clearAll = document.getElementById("btn-clearAll");
+
+// Initialize the display
+document.addEventListener("DOMContentLoaded", function() {
+    addButton.addEventListener("click", addTask);
+    taskInput.addEventListener('keydown', function(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            addTask();
+        }
+    });
+    btnFilter.addEventListener("click", filterTask);
+    clearAll.addEventListener("click", clearAllTasks);
+    displayTasks();
+});
+
+function addTask() {
+    const newTask = taskInput.value.trim();
+    if (newTask !== "") {
+        todo.push({
+            text: newTask,
+            disabled: false,
+        });
+        displayTasks();
+    }
+}
+
+function displayTasks() {
+
+}
+
+function filterTask() {
+
+}
+
+function clearAllTasks() {
+
 }
