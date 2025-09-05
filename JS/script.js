02cfe3d19b3442b5e44fce84f5cf43a5cbf2ebc4
Miel-Code <jilanrokan@gmail.com>
2025-09-05 13:45:35 +0700

    Enhance UI styles and implement input validation for task management

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+function validateInput(){
+    const inputField = document.getElementById("taskInput").value;
+    const dateField = document.getElementById("dateInput").value;
+
+    console.log('todo', inputField);
+    console.log('due date', dateField);
+}
